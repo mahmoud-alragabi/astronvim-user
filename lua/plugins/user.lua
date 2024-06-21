aebfcd1386961e32e765944acafaba99a3efec46
MedoAngel <actionman14.ma@gmail.com>
2024-06-21 09:24:12 +0100

    add leetcode.nvim plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -8,6 +8,23 @@ return {
 
   "lunarvim/darkplus.nvim",
   {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        -- configuration goes here
+    },
+  },
+  {
     "AstroNvim/astroui",
     opts = {
       colorscheme = "darkplus"
